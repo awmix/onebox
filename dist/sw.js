500bf84e09369d83b346abef0630782fd2b431dc
secoder <crisimple@MacPro.local>
2026-09-14 23:27:00 +0800

    fix: remove bottom content spacer

--- dist/sw.js
+++ dist/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'onebox-v62';
-const APP_SHELL = ['./', 'index.html', 'style.css?v=2.18.16.1', 'app.js?v=2.18.16.1', 'calendar-data.js?v=2.18.16.1', 'manifest.webmanifest', 'icons/icon.svg', 'icons/icon-180.png', 'icons/icon-192.png', 'icons/icon-512.png', 'icons/icon-dark-180.png', 'icons/icon-dark-192.png', 'icons/icon-dark-512.png', 'icons/bell.svg', 'icons/bell-192.png'];
+const CACHE = 'onebox-v63';
+const APP_SHELL = ['./', 'index.html', 'style.css?v=2.18.17.1', 'app.js?v=2.18.17.1', 'calendar-data.js?v=2.18.17.1', 'manifest.webmanifest', 'icons/icon.svg', 'icons/icon-180.png', 'icons/icon-192.png', 'icons/icon-512.png', 'icons/icon-dark-180.png', 'icons/icon-dark-192.png', 'icons/icon-dark-512.png', 'icons/bell.svg', 'icons/bell-192.png'];
 const OPEN_METEO = /(^|\.)open-meteo\.com$/;
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
