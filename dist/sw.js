20c96d5931fd6b4e919f9c065aba7fcb38d3d4fa
secoder <crisimple@MacPro.local>
2026-09-15 19:56:08 +0800

    fix: neutralize dark mode surfaces

--- dist/sw.js
+++ dist/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'onebox-v96';
-const APP_SHELL = ['./', 'index.html', 'style.css?v=2.18.50.1', 'app.js?v=2.18.50.1', 'calendar-data.js?v=2.18.50.1', 'manifest.webmanifest', 'icons/icon.svg', 'icons/icon-180.png', 'icons/icon-192.png', 'icons/icon-512.png', 'icons/icon-dark-180.png', 'icons/icon-dark-192.png', 'icons/icon-dark-512.png', 'icons/bell.svg', 'icons/bell-192.png'];
+const CACHE = 'onebox-v97';
+const APP_SHELL = ['./', 'index.html', 'style.css?v=2.18.51.1', 'app.js?v=2.18.51.1', 'calendar-data.js?v=2.18.51.1', 'manifest.webmanifest', 'icons/icon.svg', 'icons/icon-180.png', 'icons/icon-192.png', 'icons/icon-512.png', 'icons/icon-dark-180.png', 'icons/icon-dark-192.png', 'icons/icon-dark-512.png', 'icons/bell.svg', 'icons/bell-192.png'];
 const OPEN_METEO = /(^|\.)open-meteo\.com$/;
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
