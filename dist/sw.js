696659b43f1b1ebf19f1dc39b9bdf6121816492a
secoder <crisimple@MacPro.local>
2026-09-15 12:31:36 +0800

    deploy: redesign mobile converter card

--- dist/sw.js
+++ dist/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'onebox-v77';
-const APP_SHELL = ['./', 'index.html', 'style.css?v=2.18.31.1', 'app.js?v=2.18.31.1', 'calendar-data.js?v=2.18.31.1', 'manifest.webmanifest', 'icons/icon.svg', 'icons/icon-180.png', 'icons/icon-192.png', 'icons/icon-512.png', 'icons/icon-dark-180.png', 'icons/icon-dark-192.png', 'icons/icon-dark-512.png', 'icons/bell.svg', 'icons/bell-192.png'];
+const CACHE = 'onebox-v78';
+const APP_SHELL = ['./', 'index.html', 'style.css?v=2.18.32.1', 'app.js?v=2.18.32.1', 'calendar-data.js?v=2.18.32.1', 'manifest.webmanifest', 'icons/icon.svg', 'icons/icon-180.png', 'icons/icon-192.png', 'icons/icon-512.png', 'icons/icon-dark-180.png', 'icons/icon-dark-192.png', 'icons/icon-dark-512.png', 'icons/bell.svg', 'icons/bell-192.png'];
 const OPEN_METEO = /(^|\.)open-meteo\.com$/;
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
