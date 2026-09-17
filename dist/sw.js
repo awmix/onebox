8d44d0781c160c9b3b28a0239c6ea34e8e146231
secoder <crisimple@MacPro.local>
2026-09-17 12:29:12 +0800

    publish OneBox 2.18.108

--- dist/sw.js
+++ dist/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'onebox-v155';
-const APP_SHELL = ['./', 'index.html', 'style.css?v=2.18.107', 'app.js?v=2.18.107', 'calendar-data.js?v=2.18.107', 'manifest.webmanifest', 'icons/icon.svg', 'icons/icon-180.png', 'icons/icon-192.png', 'icons/icon-512.png', 'icons/icon-dark-180.png', 'icons/icon-dark-192.png', 'icons/icon-dark-512.png', 'icons/bell.svg', 'icons/bell-192.png', 'icons/weibo.png', 'icons/guancha.svg', 'icons/bilibili.svg'];
+const CACHE = 'onebox-v156';
+const APP_SHELL = ['./', 'index.html', 'style.css?v=2.18.108', 'app.js?v=2.18.108', 'calendar-data.js?v=2.18.108', 'manifest.webmanifest', 'icons/icon.svg', 'icons/icon-180.png', 'icons/icon-192.png', 'icons/icon-512.png', 'icons/icon-dark-180.png', 'icons/icon-dark-192.png', 'icons/icon-dark-512.png', 'icons/bell.svg', 'icons/bell-192.png', 'icons/weibo.png', 'icons/guancha.png?v=2.18.108', 'icons/bilibili.svg'];
 const OPEN_METEO = /(^|\.)open-meteo\.com$/;
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
